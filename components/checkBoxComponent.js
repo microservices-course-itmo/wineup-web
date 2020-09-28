0ed0b8dfedf3212cb6b22c68e0e6619fded52455
ptangra <ptangra@bitbucket.org>
2020-09-28 19:15:33 +0300

    checkbox component

---
+++ components/checkBoxComponent.js
@@ -0,0 +1,35 @@
+import React from 'react'
+// eslint-disable-next-line
+import PropTypes from 'prop-types'
+
+const Checkbox = ({ label, selected, onChange }) => {
+  const handleChange = event => {
+    const { selected } = event.target
+    onChange(selected)
+  }
+
+  return (
+    <div className='form-group form-check'>
+      <label htmlFor='check'>
+        <input
+          id='check'
+          type='checkbox'
+          defaultChecked={selected}
+          value={selected}
+          onChange={handleChange}
+        />
+        {label}
+      </label>
+    </div>
+  )
+  // eslint-disable-next-line
+}
+
+Checkbox.propTypes = {
+  selected: PropTypes.bool.isRequired,
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  // eslint-disable-next-line
+}
+
+export default Checkbox
