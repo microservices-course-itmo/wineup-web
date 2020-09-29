ea5cfe4500cca6908ad6ff5d2a3d1d43246517ec
red.213 <red.112141@gmail.com>
2020-09-29 19:05:00 +0700

    added storybook to project

---
+++ components/checkBoxComponent.stories.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Checkbox } from './checkBoxComponent'
+
+export default {
+  title: 'CheckBox',
+  component: Checkbox,
+}
+
+const Template = args => <Checkbox {...args} />
+
+export const CheckBox = Template.bind({})
+Checkbox.args = {}
---
+++ components/search-component.stories.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Search } from './search-component'
+
+export default {
+  title: 'Search',
+  component: Search,
+}
+
+const Template = args => <Search {...args} />
+
+export const search = Template.bind({})
+search.args = {}
