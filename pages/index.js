1f28acf2c4d22d9da1a9002e3494aca7a706dfd7
red.213 <red.112141@gmail.com>
2020-10-03 12:21:36 +0700

    added storybook to project with pic

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,7 @@
-
-const { default: WineCard } = require("../components/winecard")
-
 const Home = () => {
   return (
     <>
-      <WineCard
-  imageSrc='https://amwine.ru/upload/resize_cache/iblock/b8b/620_620_1/b8b1bb64748968fe374765a9f6dc2738.png'
-  info={{
-    shop: 'Ароматный мир',
-    name: 'Estate Vineyards Sauvignon Blanc',
-    about: 'Красное, полусладкое',
-    country: { code: 'it', name: 'Италия' },
-    size: 0.75,
-    year: 2011,
-    fitsPercent: 75,
-    stars: 4,
-    price: '1200',
-    discount: {
-      price: '900',
-      percent: 12,
-    },
-  }}
-  isLiked
-/>
+      <h1>Home page</h1>
     </>
   )
 }
