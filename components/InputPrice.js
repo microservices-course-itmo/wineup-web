e7a9babce4867645080b2e71343bd1bed1e8f514
lerese58 <lerese5858>
2020-10-23 23:03:09 +0300

    correct component InputPrice.js

--- components/InputPrice.js
+++ components/InputPrice.js
@@ -1,44 +1,42 @@
 /**
- * @param {string} labelFrom
- * @param {string} labelTo
- * @param {number} placeholderFrom
- * @param {number} placeholderTo
+ * @param {Object} inputFrom
+ * @param {Object} inputTo
  * @param {symbol} currency
  */
-const InputPrice = ({
-  labelFrom = 'FROM',
-  labelTo = 'TO',
-  placeholderFrom = 0,
-  placeholderTo = 10000,
-  currency = '₹',
-}) => {
+const InputPrice = ({ inputFrom, inputTo, currency = '₹' }) => {
   return (
     <>
       <div className='row-direction item-gap'>
-        <div className='column-direction color'>
-          <label htmlFor='input'>{labelFrom}</label>
+        <div className='column-direction'>
+          <label htmlFor={inputFrom.id} className='color'>
+            {inputFrom.textLabel}
+          </label>
           <div className='row-direction'>
             <input
-              id='input'
+              id={inputFrom.id}
+              name={inputFrom.name}
               className='input-price color'
               type='number'
-              placeholder={placeholderFrom}
+              placeholder={inputFrom.defaultValue}
             />
-            <label className='symbol' htmlFor='input'>
+            <label className='symbol color' htmlFor={inputFrom.id}>
               {currency}
             </label>
           </div>
         </div>
-        <div className='column-direction color'>
-          <label htmlFor='inputTo'>{labelTo}</label>
+        <div className='column-direction'>
+          <label htmlFor='inputTo' className='color'>
+            {inputTo.textLabel}
+          </label>
           <div className='row-direction'>
             <input
-              id='inputTo'
+              id={inputTo.id}
+              name={inputTo.name}
               className='input-price color'
               type='number'
-              placeholder={placeholderTo}
+              placeholder={inputTo.defaultValue}
             />
-            <label className='symbol' htmlFor='inputTo'>
+            <label className='symbol color' htmlFor={inputTo.id}>
               {currency}
             </label>
           </div>
@@ -46,9 +44,6 @@ const InputPrice = ({
       </div>
       <style jsx>
         {`
-          .backColor {
-            background-color: black;
-          }
           .column-direction {
             display: inline-flex;
             flex-direction: column;
@@ -65,16 +60,32 @@ const InputPrice = ({
             right: 15px;
             font-weight: bold;
             font-size: 24px;
+            background: none;
           }
-          .color {
+          .column-direction .color {
+            color: #9e9e9e;
+          }
+          .column-direction:focus .color,
+          .column-direction:hover .color {
             color: white;
           }
           .input-price {
             -moz-appearance: textfield;
             border: none;
             background: none;
-            border-bottom: 2px solid #666666;
-            border-color: white;
+            outline: none;
+            width: 138px;
+            border-bottom: 2px #9e9e9e solid;
+            color: white;
+          }
+          .input-price:hover,
+          .input-price:focus {
+            -moz-appearance: number-input;
+          }
+          .input-price::-webkit-inner-spin-button,
+          .input-price::-webkit-outer-spin-button {
+            -webkit-appearance: none;
+            margin: 0;
           }
         `}
       </style>
