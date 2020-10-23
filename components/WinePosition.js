4f6abc1ccc298c73eecef6718ec7befb395c12a4
Vakhtang Karchava <heyllog@gmail.com>
2020-10-23 12:29:54 +0300

    delete examples

--- components/WinePosition.js
+++ components/WinePosition.js
@@ -247,27 +247,3 @@ const WinePosition = ({ imageSrc, info, color = 0 }) => (
 )
 
 export default WinePosition
-
-// Пример:
-// <WinePosition
-//   imageSrc='https://amwine.ru/upload/resize_cache/iblock/7bc/620_620_1/7bcaa8fad7ebb211cbcda8a27b5382ba.png'
-//   info={{
-//     name: 'Estate Vineyards Sauvignon Blanc',
-//     grape: 'Арени',
-//     size: 0.75,
-//     country: 'Португалия',
-//     sugar: 'сухое',
-//     color: 'красное',
-//     shop: 'Ароматный мир',
-//     alcohol: 12,
-//     brand: 'Gevorkian Winery',
-//     year: 2011,
-//     fitsPercent: 75,
-//     stars: 3,
-//     price: '1200',
-//     discount: {
-//     price: '900',
-//       percent: 12,
-//   },
-// }}
-// />
