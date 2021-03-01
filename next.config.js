2b0ccf43e46335ce996ab39c131737feefd008c9
ptangra <38228645+ptangra@users.noreply.github.com>
2021-03-01 23:30:03 +0200

    folder to deploy to changed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.export = {
   //   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   //   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   basePath: '/wineup-web',
-  assetPrefix: '/wineup-web',
+  assetPrefix: isProd
+    ? 'https://cdn.statically.io/gh/microservices-course-itmo/wineup-web/gh-pages/'
+    : '',
 }
