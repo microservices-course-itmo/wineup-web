c3023b14668a014bdcf89c8ef308c0eded6f39b0
ptangra <38228645+ptangra@users.noreply.github.com>
2021-03-01 23:22:40 +0200

    folder to deploy to changed

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 module.export = {
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  //   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  //   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  basePath: '/wineup-web',
+  assetPrefix: '/wineup-web',
 }
