b83c89bbb60e8300babc3131c4e45f2a8159e991
ptangra <38228645+ptangra@users.noreply.github.com>
2021-03-08 18:12:26 +0200

    remoe assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
-const pathPrefix = process.env.NODE_ENV === 'production' ? '' : ''
+const pathPrefix = process.env.NODE_ENV === 'production' ? '/wineup-web' : ''
 
 module.exports = {
   assetPrefix: pathPrefix,
+  basePath: '/wineup-web',
   env: {
     pathPrefix,
   },
