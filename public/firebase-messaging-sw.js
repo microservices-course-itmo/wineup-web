729a5f7a5256ac31ae2104e4ec83b8529e7d142b
StanislavMyakishev <masiff-s@live.ru>
2021-04-04 23:11:40 +0300

    [wineup-76-notifications] firebase init fix

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -3,13 +3,13 @@ importScripts('https://www.gstatic.com/firebasejs/8.3.2/firebase-app.js')
 importScripts('https://www.gstatic.com/firebasejs/8.3.2/firebase-messaging.js')
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCExaLzKWdhhr_1IRPe1NIFdR7Uor12LTI',
-  authDomain: 'testfire-1bc2f.firebaseapp.com',
-  databaseURL: 'https://testfire-1bc2f.firebaseio.com',
-  projectId: 'testfire-1bc2f',
-  storageBucket: 'testfire-1bc2f.appspot.com',
-  messagingSenderId: '243423113529',
-  appId: '1:243423113529:web:d363f5aa8bf5ca297bf8e8',
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DB_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_KEY,
+  appId: process.env.FIREBASE_APP_ID,
 }
 
 firebase.initializeApp(firebaseConfig)
